08d00831032a465b6c3b85bada291af99d56ddf0
Ezia <esia.belbachir@protonmail.com>
2018-06-15 23:10:27 -0400

    Make training on basic and simpler_basic scenarios

--- src/qlearning-vizdoom.py
+++ src/qlearning-vizdoom.py
@@ -29,8 +29,8 @@ test_episodes_per_epoch = 100
 
 # Other parameters
 frame_repeat = 12
-resolution = (30, 45)
-screen_channels = 1
+resolution = (60, 80)
+screen_channels = 3
 episodes_to_watch = 10
 
 save_model = True
@@ -38,7 +38,7 @@ load_model = False
 skip_learning = False
 
 # Configuration file path
-config_file = "simpler_basic"
+config_file = "basic"
 config_file_path = "scenarios/" + config_file + ".cfg"
 
 # savefile paths
@@ -59,13 +59,17 @@ def save_simulation_parameters():
         file.write('frame repeat=' + str(frame_repeat) + '\n')
         file.write('resolution=' + str(resolution) + '\n')
         file.write('screen channels=' + str(screen_channels) + '\n')
+        file.write('optimizer=' + str(optimizer.get_name()) + '\n')
+        file.write('\n------------VIZDOOM CONFIG FILE------------\n\n')
+        with open(config_file_path, 'r') as configFile:
+            lines = configFile.readlines()
+            file.writelines(lines)
 
 # Converts and down-samples the input image
 def preprocess(img):
     img = skimage.transform.resize(img, list(resolution) + [screen_channels])
     img = img.astype(np.float32)
     return img
-
 
 class ReplayMemory:
     def __init__(self, capacity):
@@ -95,6 +99,18 @@ class ReplayMemory:
         i = sample(range(0, self.size), sample_size)
         return self.s1[i], self.a[i], self.s2[i], self.isterminal[i], self.r[i]
 
+def variable_summaries(tensor):
+  """Attach a lot of summaries to a Tensor (for TensorBoard visualization)."""
+  with tf.name_scope('summaries'):
+    mean = tf.reduce_mean(tensor)
+    tf.summary.scalar('mean', mean)
+    with tf.name_scope('stddev'):
+      stddev = tf.sqrt(tf.reduce_mean(tf.square(tensor - mean)))
+    tf.summary.scalar('stddev', stddev)
+    tf.summary.scalar('max', tf.reduce_max(tensor))
+    tf.summary.scalar('min', tf.reduce_min(tensor))
+    tf.summary.histogram('histogram', tensor)
+
 
 def create_network(session, available_actions_count):
     # Create the input variables
@@ -103,28 +119,74 @@ def create_network(session, available_actions_count):
     target_q_ = tf.placeholder(tf.float32, [None, available_actions_count], name="TargetQ")
 
     # Add 2 convolutional layers with ReLu activation
-    conv1 = tf.contrib.layers.convolution2d(s1_, num_outputs=8, kernel_size=[6, 6], stride=[3, 3],
+    # conv1 = tf.contrib.layers.convolution2d(s1_, num_outputs=32, kernel_size=[7, 7], 
+    #                                         stride=[2, 2],
+    #                                         activation_fn=tf.nn.relu,
+    #                                         weights_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
+    #                                         biases_initializer=tf.constant_initializer(0.1))
+    # conv2 = tf.contrib.layers.convolution2d(conv1, num_outputs=64, kernel_size=[7, 7], 
+    #                                         stride=[2, 2],
+    #                                         activation_fn=tf.nn.relu,
+    #                                         weights_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
+    #                                         biases_initializer=tf.constant_initializer(0.1))
+    # maxPool1 = tf.contrib.layers.max_pool2d(conv2, kernel_size=[3, 3], stride=[2, 2])
+    # conv3 = tf.contrib.layers.convolution2d(maxPool1, num_outputs=128, kernel_size=[3, 3], 
+    #                                         stride=[1, 1],
+    #                                         activation_fn=tf.nn.relu,
+    #                                         weights_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
+    #                                         biases_initializer=tf.constant_initializer(0.1))
+    # maxPool2 = tf.contrib.layers.max_pool2d(conv3, kernel_size=[3, 3], stride=[2, 2])
+    # conv4 = tf.contrib.layers.convolution2d(maxPool2, num_outputs=192, kernel_size=[3, 3], 
+    #                                         stride=[1, 1],
+    #                                         activation_fn=tf.nn.relu,
+    #                                         weights_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
+    #                                         biases_initializer=tf.constant_initializer(0.1))
+    # conv4_flat = tf.contrib.layers.flatten(conv4)
+    # fc1 = tf.contrib.layers.fully_connected(conv4_flat, num_outputs=256,                  
+    #                                         activation_fn=tf.nn.relu,
+    #                                         weights_initializer=tf.contrib.layers.xavier_initializer(),
+    #                                         biases_initializer=tf.constant_initializer(0.1))
+
+    # q = tf.contrib.layers.fully_connected(fc1, num_outputs=available_actions_count, 
+    #                                         activation_fn=None,
+    #                                         weights_initializer=tf.contrib.layers.xavier_initializer(),
+    #                                         biases_initializer=tf.constant_initializer(0.1))
+    # best_a = tf.argmax(q, 1)
+
+    conv1 = tf.contrib.layers.convolution2d(s1_, num_outputs=32, kernel_size=[8, 8], 
+                                            stride=[4, 4],
                                             activation_fn=tf.nn.relu,
                                             weights_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
                                             biases_initializer=tf.constant_initializer(0.1))
-    conv2 = tf.contrib.layers.convolution2d(conv1, num_outputs=8, kernel_size=[3, 3], stride=[2, 2],
+    conv2 = tf.contrib.layers.convolution2d(conv1, num_outputs=64, kernel_size=[4, 4], 
+                                            stride=[2, 2],
                                             activation_fn=tf.nn.relu,
                                             weights_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
                                             biases_initializer=tf.constant_initializer(0.1))
-    conv2_flat = tf.contrib.layers.flatten(conv2)
-    fc1 = tf.contrib.layers.fully_connected(conv2_flat, num_outputs=128, activation_fn=tf.nn.relu,
+    conv3 = tf.contrib.layers.convolution2d(conv2, num_outputs=64, kernel_size=[3, 3], 
+                                            stride=[1, 1],
+                                            activation_fn=tf.nn.relu,
+                                            weights_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
+                                            biases_initializer=tf.constant_initializer(0.1))
+    conv3_flat = tf.contrib.layers.flatten(conv3)
+    fc1 = tf.contrib.layers.fully_connected(conv3_flat, num_outputs=512,                  
+                                            activation_fn=tf.nn.relu,
                                             weights_initializer=tf.contrib.layers.xavier_initializer(),
                                             biases_initializer=tf.constant_initializer(0.1))
 
-    q = tf.contrib.layers.fully_connected(fc1, num_outputs=available_actions_count, activation_fn=None,
-                                          weights_initializer=tf.contrib.layers.xavier_initializer(),
-                                          biases_initializer=tf.constant_initializer(0.1))
+    q = tf.contrib.layers.fully_connected(fc1, num_outputs=available_actions_count, 
+                                            activation_fn=None,
+                                            weights_initializer=tf.contrib.layers.xavier_initializer(),
+                                            biases_initializer=tf.constant_initializer(0.1))
     best_a = tf.argmax(q, 1)
 
-    loss = tf.losses.mean_squared_error(q, target_q_)
-    tf.summary.scalar('loss', loss)
+
+    with tf.name_scope('loss'):
+        loss = tf.losses.mean_squared_error(q, target_q_)
+        tf.summary.scalar('value', loss)
 
     optimizer = tf.train.RMSPropOptimizer(learning_rate)
+
     # Update the parameters according to the computed gradient using RMSProp.
     train_step = optimizer.minimize(loss)
 
@@ -143,7 +205,7 @@ def create_network(session, available_actions_count):
     def function_simple_get_best_action(state):
         return function_get_best_action(state.reshape([1, resolution[0], resolution[1], screen_channels]))[0]
 
-    return function_learn, function_get_q_values, function_simple_get_best_action
+    return function_learn, function_get_q_values, function_simple_get_best_action, optimizer
 
 
 def learn_from_memory():
@@ -186,6 +248,10 @@ def perform_learning_step(epoch):
 
     # With probability eps make a random action.
     eps = exploration_rate(epoch)
+
+    explorationRateSummary.value[0].simple_value = eps
+    train_writer.add_summary(explorationRateSummary, train_steps_since_start)
+
     if random() <= eps:
         a = randint(0, len(actions) - 1)
     else:
@@ -208,20 +274,15 @@ def initialize_vizdoom(config_file_path):
     game = DoomGame()
     game.load_config(config_file_path)
     game.set_window_visible(True)
-    game.set_mode(Mode.PLAYER)
-    game.set_screen_format(ScreenFormat.GRAY8)
-    game.set_screen_resolution(ScreenResolution.RES_640X480)
     game.init()
     print("Doom initialized.")
     return game
 
 
 if __name__ == '__main__':
-    # Save parameters of training
-    save_simulation_parameters()
-
     # Create Doom instance
     game = initialize_vizdoom(config_file_path)
+
 
     # Action = which buttons are pressed
     n = game.get_available_buttons_size()
@@ -231,7 +292,7 @@ if __name__ == '__main__':
     memory = ReplayMemory(capacity=replay_memory_size)
 
     session = tf.Session()
-    learn, get_q_values, get_best_action = create_network(session, len(actions))
+    learn, get_q_values, get_best_action, optimizer = create_network(session, len(actions))
     saver = tf.train.Saver()
     if load_model:
         print("Loading model from: ", model_savefile)
@@ -241,11 +302,20 @@ if __name__ == '__main__':
         session.run(init)
     print("Starting the training!")
 
-    # tensor board init
-    scoreSummary = tf.Summary(value=[tf.Summary.Value(tag="score", simple_value=0)])
+    # Save parameters of training
+    save_simulation_parameters()
+
+    # score summaries
+    trainScoreSummary = tf.Summary(value=[tf.Summary.Value(tag="score/training value", simple_value=0)])
+    testScoreSummary = tf.Summary(value=[tf.Summary.Value(tag="score/test value", simple_value=0)])
+    explorationRateSummary = tf.Summary(value=[tf.Summary.Value(tag="exploration rate/value", simple_value=0)])
+    trainOrTestSummary = tf.Summary(value=[tf.Summary.Value(tag="process/train(1) or test(-1)", simple_value=0)])
     train_steps_since_start = 0
-    train_episodes_since_start = 0
+
+    # tensor board summary merge
     merged = tf.summary.merge_all()
+
+    # file writer
     train_writer = tf.summary.FileWriter(tensorboard_savefile, session.graph)
 
     time_start = time()
@@ -259,15 +329,18 @@ if __name__ == '__main__':
             game.new_episode()
             for learning_step in trange(learning_steps_per_epoch, leave=False):
                 perform_learning_step(epoch)
-                train_steps_since_start += 1
                 if game.is_episode_finished():
                     score = game.get_total_reward()
-                    scoreSummary.value[0].simple_value = score
-                    train_writer.add_summary(scoreSummary, train_episodes_since_start)
                     train_scores.append(score)
                     game.new_episode()
                     train_episodes_finished += 1
-                    train_episodes_since_start += 1
+
+                    trainScoreSummary.value[0].simple_value = score
+                    train_writer.add_summary(trainScoreSummary, train_steps_since_start)
+                
+                trainOrTestSummary.value[0].simple_value = 1
+                train_writer.add_summary(trainOrTestSummary, train_steps_since_start)
+                train_steps_since_start += 1
 
             print("%d training episodes played." % train_episodes_finished)
 
@@ -286,8 +359,14 @@ if __name__ == '__main__':
                     best_action_index = get_best_action(state)
 
                     game.make_action(actions[best_action_index], frame_repeat)
+                    train_steps_since_start += 1
+                    trainOrTestSummary.value[0].simple_value = -1
+                    train_writer.add_summary(trainOrTestSummary, train_steps_since_start-1)
                 r = game.get_total_reward()
                 test_scores.append(r)
+
+                testScoreSummary.value[0].simple_value = r
+                train_writer.add_summary(testScoreSummary, train_steps_since_start-1)
 
             test_scores = np.array(test_scores)
             print("Results: mean: %.1f±%.1f," % (
